6916f255835b1ddb041a5563ad2231023990c03c
enznathan <enznathanzs@gmail.com>
2026-03-17 11:41:53 -0300

    17-03

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-function App ( ){
-  return(
-    <div>
-      <h1>Aula de React com Vite Js</h1>
-      <h2>eee</h2>
-      <h3>ee</h3>
-    </div>
-  );
+
+import { Routes, Route } from 'react-router-dom'
+
+import Home from './pages/home'
+import Exemplo01 from './pages/exemplos/ex-01';
+import Exemplo02 from './pages/exemplos/ex-02';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exemplo/1" element={<Exemplo01 />} />
+      <Route path="/exemplo/2" element={<Exemplo02 />} />
+    </Routes>
+  )
 }
+
 export default App;
